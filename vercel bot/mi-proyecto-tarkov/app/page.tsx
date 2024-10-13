793e3c70c3482e8d968f5436e6631f88afa8dfd9
jorgeyt2001 <orantes404@gmail.com>
2024-10-13 15:31:33 +0200

    Create page.tsx

---
+++ vercel bot/mi-proyecto-tarkov/app/page.tsx
@@ -0,0 +1,9 @@
+import PrecioArticulo from './ItemPrice'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <PrecioArticulo />
+    </main>
+  )
+}
